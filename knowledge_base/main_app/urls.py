2f18ff422014f0a1a88498f369c9f94b3cdfc71a
Владимир Ященко <47042283+vsyaschenko@users.noreply.github.com>
2020-01-11 14:51:00 +0300

    Basic templates created, bug fixes

--- knowledge_base/main_app/urls.py
+++ knowledge_base/main_app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import index, by_section, by_process, by_solution
+from .views import index, by_section, by_process, by_problem
 
 urlpatterns = [
     path('', index, name='index'),
     path('section/<int:section_id>/', by_section, name='by_section'),
     path('process/<int:process_id>/', by_process, name='by_process'),
-    path('soluiton/<int:solution_id>/', by_solution, name='by_solution')
+    path('problem/<int:problem_id>/', by_problem, name='by_problem')
 ]
--- knowledge_base/main_app/views.py
+++ knowledge_base/main_app/views.py
@@ -1,27 +1,43 @@
 from django.shortcuts import render
 
-from .models import Sections, Solutions, Settings
+from .models import Sections, Processes, Problems, Solutions, Settings
 
-# Create your views here.
-def index(request):
-    solutions = Solutions.objects.all()
+
+def get_default_context():
     sections = Sections.objects.all()
+    
     if Settings.objects.filter(key='company_name').count()==0:
         company_name = 'KNOWLEDGE BASE'
     else:
         company_name = Settings.objects.get(key='company_name').value
+    
+    return {'sections': sections, 
+            'company_name': company_name}
 
-    context = {'solutions': solutions, 
-                'sections': sections,
-                'company_name': company_name}
+# Create your views here.
+def index(request):
+    context = get_default_context()
+    context['processes'] = Processes.objects.all()
 
     return render(request, 'main_app/index.html', context)
 
 def by_section(request, section_id):
-    pass
+    context = get_default_context()   
+    context['processes'] = Processes.objects.filter(section=section_id)
+    context['current_section'] = Sections.objects.get(pk=section_id)
+
+    return render(request, 'main_app/by_section.html', context)
 
 def by_process(request, process_id):
-    pass
+    context = get_default_context()   
+    context['problems'] = Problems.objects.filter(process=process_id)
+    context['current_process'] = Processes.objects.get(pk=process_id)
+    
+    return render(request, 'main_app/by_process.html', context)
 
-def by_solution(request, process_id):
-    pass
+def by_problem(request, problem_id):
+    context = get_default_context()   
+    context['current_problem'] = Problems.objects.get(pk=problem_id)
+    context['current_solution'] = Solutions.objects.get(problem=problem_id)
+
+    return render(request, 'main_app/by_problem.html', context)
